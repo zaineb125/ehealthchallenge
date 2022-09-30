f8fb0fdf629000370553da1535cd84d12c44b800
ilef-malouch <malouch.ilef@gmail.com>
2022-09-30 03:50:44 +0100

    reclamation

--- e-health/src/screens/ReclamerProduit.js
+++ e-health/src/screens/ReclamerProduit.js
@@ -1,31 +1,33 @@
-import React from "react";
-import useModal from "./UseModal";
-import Modal from "./Modal";
-import {useState} from 'react';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import "../styles/reclamation.css"
+function ReclamerProduit() {
+  const [show, setShow] = useState(false);
 
-export default function ReclamerProduit() {
-  const { isShowing: isFormShowed, toggle: toggleForm } = useModal();
-  const initialState = 
-  {
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const initialState = {
     reference:'',
     type:'',
     etablissement:'',
     region:''
-  }
+   }
+   const [reference , setReference] = useState('');
+   const [type, setType] = useState('');
+   const [etablissement, setEtablissement] = useState('');
+   const [region, setRegion] = useState('');
+   const [reclamation, setReclamation] = useState(initialState);
 
-  const [reference , setReference] = useState('');
-  const [type, setType] = useState('');
-  const [etablissement, setEtblissement] = useState('');
-  const [region, setRegion] = useState('');
-  const [reclamation, setReclamation] = useState(initialState);
-
-  const handleSubmit = event => {
+   const handleSubmit = event => {
     console.log('handleSubmit ran');
-    //event.preventDefault(); // 👈️ prevent page refresh
+    event.preventDefault(); // 👈️ prevent page refresh
 
     console.log('reclamation' ,reclamation);
     // 👇️ clear all input values in the form
-    setEtblissement('');
+    setEtablissement('');
     setReference('');
     setRegion('');
     setType('');
@@ -33,78 +35,66 @@ export default function ReclamerProduit() {
 
   return (
     <>
-      <div className="App">
-        <button className="modal-toggle" onClick={toggleForm}>
-          Reclamer
-        </button>
-        <Modal
-          isShowing={isFormShowed}
-          hide={toggleForm}
-          title="Reclamer"
-        >
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <input type="text" placeholder="Etablissement" value = {etablissement} onChange={event => setEtblissement(event.target.value)}/>
-            </div>
-            <div className="form-group">
-              <input type="text" placeholder="Region" value = {region} onChange={event => setRegion(event.target.value)}/>
-            </div>
-            <div className="form-group">
-            <select type="select" value = {type} onChange={event => setType(event.target.value)}>
-              <option valeur="pharmacien">Pharmacien</option>
-              <option valeur="grossiste">Grossiste</option>
-              <option valeur="medecin">Medecin</option>
-            </select>
-            </div>
-            <div className="form-group">
-              <input 
-                type="submit" 
-                value="Confirmer" 
-                onClick={event => setReclamation({
-                  reference : reference,
-                  type:type,
-                  etablissement:etablissement,
-                  region:region
-                })} 
-              />
-            </div>
-          </form>
-        </Modal>
-      </div>
+      <Button variant="primary" onClick={handleShow}>
+        Reclamer
+      </Button>
 
-      <style jsx="true">{`
-        .App {
-          height: 100vh;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        button.modal-toggle,
-        input[type="submit"] {
-          background-color: turquoise;
-          cursor: pointer;
-          padding: 1rem 2rem;
-          text-transform: uppercase;
-          border: none;
-        }
-
-        button.modal-toggle:not(:first-child) {
-          margin-left: 10px;
-        }
-
-        .form-group {
-          margin-top: 10px;
-        }
-
-        input[type="text"],
-        input[type="password"],
-        input[type="email"] {
-          box-sizing: border-box;
-          width: 100%;
-          padding: 0.5rem 0.7rem;
-        }
-      `}</style>
+      <Modal  show={show} onHide={handleClose}>
+        <div className='cont'>
+        <Modal.Header closeButton>
+          <Modal.Title style={{fontSize:"23px"}}>Ajouter Les Details De Votre Reclamation</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+                <Form.Group >
+                    <Form.Label>Reference medicament</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Reference"
+                        autoFocus
+                        onChange={event => setReference(event.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group onChange={event => setType(event.target.value)}>
+                    <Form.Label>Type</Form.Label>
+                    <Form.Select >
+                        <option style={{color:"white"}} valeur="pharmacien">Pharmacien</option>
+                        <option style={{color:"white"}} valeur="grossiste">Grossiste</option>
+                        <option style={{color:"white"}} valeur="medecin">Medecin</option>
+                    </Form.Select>
+                </Form.Group>
+                <Form.Group onChange={event => setEtablissement(event.target.value)}>
+                <Form.Label>Etablissement</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Etablissement"
+                    autoFocus
+                />
+                </Form.Group>
+                <Form.Group onChange={event => setRegion(event.target.value)}>
+                <Form.Label>Region</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Region"
+                    autoFocus
+                />
+                </Form.Group>
+                <br></br>
+                <Button className = 'btn' variant="success" type ="submit" value="Submit" onClick={event => setReclamation({
+                    reference : reference,
+                    type:type,
+                    etablissement:etablissement,
+                    region:region
+                })} >
+                    Save Changes
+                </Button>
+            </Form>
+        </Modal.Body>
+        </div>
+       
+      </Modal>
     </>
   );
 }
+
+export default ReclamerProduit;
